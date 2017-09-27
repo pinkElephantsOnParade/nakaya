4661365a68a88956764c0652a8f6ddea06fa2c53
pinkElephantsOnParade <frompython2swallow@gmail.com>
2017-09-27 09:24:33 +0900

    add loopProcess function.

--- nakaya/system.py
+++ nakaya/system.py
@@ -1,7 +1,25 @@
 
 import sys
+import time
 
 def pathList():
     print("[Python System Path List]")
     for item in sys.path:
         print(item)    
+
+def loopProcess(func, args=(), maxRetry=100, isPrint=False, Exceptions=(Exception,)):
+    count = 0
+    dst = None
+    while (1 <= maxRetry):
+        try:
+            dst = func(*args)
+            maxRetry = -1 
+        except Exceptions as e:
+            if isPrint:
+                print(e)
+            time.sleep(5)
+            maxRetry = maxRetry - 1
+            count = count + 1
+        if isPrint:
+            print( "[" + func.__name__ +":count]" + str(count))
+    return dst
